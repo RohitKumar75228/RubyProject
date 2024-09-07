70b7efb4a6af3e08541a490c0ef66b4cd2cfb699
RohitKumar75228 <rk2828555@gmail.com>
2024-09-07 10:04:01 +0530

    Add Project

---
+++ admin/lib/spree/admin.rb
@@ -0,0 +1,21 @@
+require 'spree_core'
+
+require 'sprockets/railtie'
+
+require 'active_link_to'
+require 'countries/global'
+require 'country_select'
+require 'currency_select'
+require 'dartsass-rails'
+require 'importmap-rails'
+require 'bootstrap'
+require 'turbo-rails'
+require 'stimulus-rails'
+require 'inline_svg'
+require 'payment_icons'
+require 'responders'
+require 'tinymce-rails'
+
+require 'spree/admin/action_callbacks'
+require 'spree/admin/callbacks'
+require 'spree/admin/engine'
